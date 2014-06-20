634736b4c3d7a5e68eaa6e5ecc62acceba97ddaa
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-19 19:56:45 -0700

    Adding more filez

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,6 +1,7 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Backbone.Collection.extend({
   model: SongModel,
+  localStorage = new Store(SongQueue);
 
   initialize: function(){
     // _.bindAll(this, 'enqueue', 'dequeue', 'autoPlay');
@@ -8,22 +9,25 @@ var SongQueue = Backbone.Collection.extend({
     this.on('addSong', function() {
       if (this.length === 1) {
        this.autoPlay();
+       localStorage['queue'] = 'hey';
       }
     });
-
+    // on songModel.ended()
     this.on('ended', function(song) {
+      //remove the song that ended
       this.remove(song);
+      //call autoplay if songQueue.length >=1
       if (this.length >=1) {
         this.autoPlay();
       }
     }, this);
-
+    //remove a song if it is dequeued
     this.on('dequeue', function(song){
       this.remove(song);
     },this);
   },
 
-
+//Play whatever is at songQueue[0]
   autoPlay: function(){
     this.at(0).play();
   }
